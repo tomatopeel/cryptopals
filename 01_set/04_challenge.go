580070b61662520f9411c4ae6b2337e3f30900ee
tomatopeel <thomaso@thomaso.me>
2017-05-23 14:58:01 +0200

    01/04 complete.

--- 01_set/04_challenge.go
+++ 01_set/04_challenge.go
@@ -2,26 +2,28 @@ package main
 
 import (
 	"bufio"
+	"encoding/hex"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"sort"
 	"strings"
 )
 
 var (
 	file_url   string = "http://cryptopals.com/static/challenge-data/4.txt"
-	local_file string = "/home/tomato/secrets.txt"
+	local_file string = "secrets.txt"
 	alphabet   string = "abcdefghijklmnopqrstuvwxyz"
 	vowels     string = "aeiouAEIOU"
 	consonants string = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ"
 )
 
 type Line struct {
-	original string
-	xored    string
-	score    int
+	Secret   []byte
+	TopXored string
+	Score    int
 }
 
 func main() {
@@ -30,8 +32,11 @@ func main() {
 		file = retrieveFile(local_file, file_url)
 	}
 	results := ParseFile(file)
-	for _, line := range results {
-		fmt.Println(line.original)
+	sort.Slice(results, func(i, j int) bool { return results[i].Score > results[j].Score })
+
+	for i := 0; i < 1; i++ {
+		fmt.Printf("%d\n", results[i].Score)
+		fmt.Println(string(results[i].TopXored))
 	}
 }
 
@@ -46,12 +51,11 @@ func ParseFile(file *os.File) []Line {
 	lines := make([]Line, n)
 	scanner = bufio.NewScanner(file)
 	for i := 0; scanner.Scan(); i++ {
-		original := scanner.Text()
-		lines[i] = Line{
-			original: original,
-			xored: Xor(original)
-			top_score: Score(
+		line, err := hex.DecodeString(scanner.Text())
+		if err != nil {
+			log.Fatal(err)
 		}
+		lines[i] = Xor(line)
 	}
 
 	return lines
@@ -77,30 +81,40 @@ func retrieveFile(local string, url string) *os.File {
 	return file
 }
 
-func Xor(char int32, secret []byte) string {
+func Xor(secret []byte) Line {
+	line := Line{}
+	line.Secret = secret
 	tester := make([]byte, len(secret))
-	for i := 0; i < len(secret); i++ {
-		tester[i] = byte(char)
+
+	for a := range alphabet {
+		for i := 0; i < len(secret); i++ {
+			tester[i] = byte(a)
+		}
+		for i := range secret {
+			tester[i] ^= secret[i]
+		}
+		score := Score(tester)
+		if score > line.Score {
+			line.Score = score
+			line.TopXored = string(tester)
+		}
 	}
-	for i := range secret {
-		tester[i] ^= secret[i]
-	}
-	fmt.Printf("%d\t%s\n", Score(tester), string(tester))
-	return ""
+
+	return line
 }
 
-func Score(Line []byte) int {
+func Score(line []byte) int {
 	x := 0
-	for i := range Line {
+	for i := range line {
 		switch {
-		case strings.ContainsRune(vowels, rune(Line[i])):
+		case strings.ContainsRune(vowels, rune(line[i])):
 			x += 2
-		case strings.ContainsRune(consonants, rune(Line[i])):
+		case strings.ContainsRune(consonants, rune(line[i])):
 			for j := i - 1; j > i-4; j-- {
 				if j < 0 {
 					break
 				}
-				if strings.ContainsRune(vowels, rune(Line[j])) {
+				if strings.ContainsRune(vowels, rune(line[j])) {
 					x += 2
 					break
 				}
@@ -108,5 +122,5 @@ func Score(Line []byte) int {
 			x++
 		}
 	}
-	return (x * 1000) / len(Line)
+	return (x * 1000) / len(line)
 }
