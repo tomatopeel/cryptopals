b5eaa7e61a96ef17efd54c3f95574ebae109fc65
tomatopeel <thomaso@thomaso.me>
2017-05-24 15:40:12 +0200

    Ripped out bitwise operational code into its own library.

--- 01_set/06_challenge.go
+++ 01_set/06_challenge.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"github.com/tomatopeel/pals/bitutil"
 	"log"
 )
 
@@ -14,52 +14,9 @@ var (
 )
 
 func main() {
-	result, err := ComputeHamming([]byte(ham_a), []byte(ham_b))
+	result, err := bitutil.Hamming([]byte(ham_a), []byte(ham_b))
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(result)
-}
-
-func ComputeHamming(a, b []byte) (int, error) {
-	if len(a) != len(b) {
-		return 0, errors.New("ComputeHamming: was passed strings of differing lengths")
-	}
-	count := 0
-	for i, _ := range a {
-		a[i] ^= b[i]
-		count += CountBits(int(a[i]))
-	}
-	return count, nil
-}
-
-func CountBitsNaive(x int) int {
-	count := 0
-	for x > 0 {
-		count += x & 1
-		x >>= 1
-	}
-	if count == 0 {
-		log.Fatal("Hm")
-	}
-	return count
-}
-
-func CountBitsKernighan(x int) int {
-	count := 0
-	for x > 0 {
-		x &= x - 1
-		count++
-	}
-	return count
-}
-
-func CountBits(v int) int {
-	c := 0
-	c = v - ((v >> 1) & 0x55555555)
-	c = ((c >> 2) & 0x33333333) + (c & 0x33333333)
-	c = ((c >> 4) + c) & 0x0F0F0F0F
-	c = ((c >> 8) + c) & 0x00FF00FF
-	c = ((c >> 16) + c) & 0x0000FFFF
-	return c
 }
---
+++ bitutil/bitutil.go
@@ -0,0 +1,44 @@
+package bitutil
+
+import (
+	"errors"
+)
+
+func Hamming(a, b []byte) (int, error) {
+	if len(a) != len(b) {
+		return 0, errors.New("passed []bytes of unequal length")
+	}
+	c := 0
+	for i := 0; i < len(a); i++ {
+		c += CountBits(int(a[i] ^ b[i]))
+	}
+	return c, nil
+}
+
+func CountBitsNaive(x int) int {
+	c := 0
+	for x > 0 {
+		c += x & 1
+		x >>= 1
+	}
+	return c
+}
+
+func CountBitsKernighan(x int) int {
+	c := 0
+	for x > 0 {
+		x &= x - 1
+		c++
+	}
+	return c
+}
+
+func CountBits(v int) int {
+	c := 0
+	c = v - ((v >> 1) & 0x55555555)
+	c = ((c >> 2) & 0x33333333) + (c & 0x33333333)
+	c = ((c >> 4) + c) & 0x0F0F0F0F
+	c = ((c >> 8) + c) & 0x00FF00FF
+	c = ((c >> 16) + c) & 0x0000FFFF
+	return c
+}
