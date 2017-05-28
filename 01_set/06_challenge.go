8f1c8f66618cbe7ba9cc724f495c49b38e4280b3
tomatopeel <thomaso@thomaso.me>
2017-05-28 18:28:22 +0200

    First shot at the algorithm for 01/06 implemented.
    No obvious result for the keysize with this, need to re-implement
    to operate on all the bytes of the data with whichever keysize.

--- 01_set/06_challenge.go
+++ 01_set/06_challenge.go
@@ -15,6 +15,8 @@ var (
 	local_file  string = "secrets_01_06.txt"
 )
 
+type Blocks [][]byte
+
 func main() {
 	f, err := os.Open(local_file)
 	if err != nil {
@@ -25,23 +27,30 @@ func main() {
 		}
 	}
 
-	d := base64.NewDecoder(base64.StdEncoding, f)
-
+	decoder := base64.NewDecoder(base64.StdEncoding, f)
 	for i := 2; i <= 40; i++ {
-		HamBytes(d, i)
+		fmt.Println(Ham(decoder, 6, i))
 	}
 }
 
-func HamBytes(reader io.Reader, k int) int {
-	a := make([]byte, k)
-	b := make([]byte, k)
-	reader.Read(a)
-	reader.Read(b)
-	result, err := bitutils.Hamming(a, b)
-	if err != nil {
-		log.Fatal(err)
+func Ham(reader io.Reader, keysize, n_blocks int) float64 {
+	ham := float64(0)
+	blocks := make([][]byte, n_blocks)
+	for i := range blocks {
+		blocks[i] = make([]byte, keysize)
+		_, err := reader.Read(blocks[i])
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
-	result /= k
-	fmt.Println(result)
-	return result
+	for i := range blocks {
+		for j := i + 1; j < len(blocks); j++ {
+			result, err := bitutils.Hamming(blocks[i], blocks[j])
+			if err != nil {
+				log.Fatal(err)
+			}
+			ham += float64(result / keysize)
+		}
+	}
+	return ham / float64(n_blocks)
 }
