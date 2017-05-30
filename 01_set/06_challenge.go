d3de8c4f2d8256c7a9ccaa4a882651747fd9858d
tomatopeel <thomaso@thomaso.me>
2017-05-30 13:17:47 +0200

    For keysize 2, the Ham function's reader b  is only reading 1 byte.

--- 01_set/06_challenge.go
+++ 01_set/06_challenge.go
@@ -26,7 +26,31 @@ func main() {
 	defer f.Close()
 
 	keysize := FindKeySize(1, 50, f)
-	log.Println(keysize)
+	blocks := Blocks(keysize, f)
+	transd := make([][]byte, keysize)
+
+	for _, block := range blocks {
+		for j, byte := range block {
+			transd[j] = append(transd[j], byte)
+		}
+	}
+}
+
+func Blocks(keysize int, f *os.File) (blocks [][]byte) {
+	decoder := base64.NewDecoder(base64.StdEncoding, f)
+
+	for {
+		block := make([]byte, keysize)
+		n, err := decoder.Read(block)
+		if n == 0 {
+			return
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		blocks = append(blocks, block)
+	}
+	return
 }
 
 func FindKeySize(x, y int, f *os.File) int {
@@ -34,17 +58,25 @@ func FindKeySize(x, y int, f *os.File) int {
 		ham     float64
 		keysize int
 	)
+
 	for i := x; i <= y; i++ {
 		decoder := base64.NewDecoder(base64.StdEncoding, f)
+
 		temp_ham, err := Ham(decoder, i)
 		if err != nil {
 			log.Fatal(err)
 		}
+
 		if temp_ham < ham || ham == 0 {
 			ham = temp_ham
 			keysize = i
 		}
-		f.Seek(0, 0)
+
+		n, err := f.Seek(0, 0)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("seek's n: %d", n)
 	}
 	return keysize
 }
@@ -55,11 +87,19 @@ func Ham(reader io.Reader, keysize int) (float64, error) {
 	i := 0
 	for {
 		n, err := reader.Read(a)
-		if n == 0 {
+		if err != nil && err != io.EOF {
+			log.Fatal(err)
+		}
+		log.Printf("a: %x\tn=%d\tkeysize=%d", a, n, keysize)
+		if n != keysize {
 			break
 		}
 		n, err = reader.Read(b)
-		if n == 0 {
+		if err != nil && err != io.EOF {
+			log.Fatal(err)
+		}
+		log.Printf("b: %x\tn=%d\tkeysize=%d", b, n, keysize)
+		if n != keysize {
 			break
 		}
 		result, err := bitutils.Hamming(a, b)
@@ -70,5 +110,6 @@ func Ham(reader io.Reader, keysize int) (float64, error) {
 		ham += float64(normalised)
 		i++
 	}
+	log.Println("and out...")
 	return ham / float64(i), nil
 }
